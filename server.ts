8fa8d560c26d47f7f6689364e72a109c3ab35b51
hiuhuedd <hiuhuedd@gmail.com>
2024-12-16 05:02:22 -0800

    first commit3

--- server.ts
+++ server.ts
@@ -1,7 +1,7 @@
 import http from "http";
 import { app } from "./app";
 const server = http.createServer(app);
-const PORT = process.env.PORT || 5000;
+const PORT =  5000;
 // create server
 server.listen(PORT, () => {
   console.log(`Server is connected with port ${PORT}`);
